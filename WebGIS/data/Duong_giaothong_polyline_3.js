df9207f787f6d5eb0f64f41c205270e63afd7100
thienphan97 <50607697+thienphan97@users.noreply.github.com>
2019-05-14 11:48:39 +0700

    add new file

---
+++ WebGIS/data/Duong_giaothong_polyline_3.js
@@ -0,0 +1,20 @@
+var json_Duong_giaothong_polyline_3 = {
+"type": "FeatureCollection",
+"name": "Duong_giaothong_polyline_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "ID": "0", "Tên_Ðuong": "0" }, "geometry": { "type": "LineString", "coordinates": [ [ 0.201252, 0.092903 ], [ 0.263604, 0.063213 ], [ 0.346753, 0.022395 ] ] } },
+{ "type": "Feature", "properties": { "ID": "1", "Tên_Ðuong": "Ðuong sô 2" }, "geometry": { "type": "LineString", "coordinates": [ [ 0.555969, 0.589004 ], [ 0.265533, 0.058586 ] ] } },
+{ "type": "Feature", "properties": { "ID": "2", "Tên_Ðuong": "0" }, "geometry": { "type": "LineString", "coordinates": [ [ 0.525571, 0.354141 ], [ 0.664552, 0.278094 ], [ 0.738326, 0.3233 ], [ 0.775787, 0.336191 ] ] } },
+{ "type": "Feature", "properties": { "ID": "3", "Tên_Ðuong": "0" }, "geometry": { "type": "LineString", "coordinates": [ [ 0.261128, 0.384136 ], [ 0.361335, 0.330344 ], [ 0.401069, 0.306238 ], [ 0.479479, 0.269126 ], [ 0.599582, 0.200506 ], [ 0.65622, 0.266084 ], [ 0.668292, 0.279938 ] ] } },
+{ "type": "Feature", "properties": { "ID": "4", "Tên_Ðuong": "0" }, "geometry": { "type": "LineString", "coordinates": [ [ 0.320838, 0.16116 ], [ 0.262088, 0.205641 ], [ 0.242561, 0.274108 ], [ 0.253053, 0.34808 ], [ 0.26008, 0.394311 ] ] } },
+{ "type": "Feature", "properties": { "ID": "5", "Tên_Ðuong": "0" }, "geometry": { "type": "LineString", "coordinates": [ [ 0.168693, 0.326011 ], [ 0.244047, 0.3111 ] ] } },
+{ "type": "Feature", "properties": { "ID": "6", "Tên_Ðuong": "Ðuong sô 7" }, "geometry": { "type": "LineString", "coordinates": [ [ 0.322304, 0.729933 ], [ 0.481053, 0.651082 ], [ 0.561422, 0.606568 ], [ 0.639931, 0.560207 ], [ 0.631707, 0.538023 ], [ 0.48408, 0.278367 ], [ 0.37447, 0.06767 ], [ 0.347723, 0.019619 ] ] } },
+{ "type": "Feature", "properties": { "ID": "7", "Tên_Ðuong": "Ðuong sô 5" }, "geometry": { "type": "LineString", "coordinates": [ [ 0.436999, 0.374622 ], [ 0.231936, 0.477589 ], [ 0.230006, 0.482216 ] ] } },
+{ "type": "Feature", "properties": { "ID": "8", "Tên_Ðuong": "Ðuong sô 10" }, "geometry": { "type": "LineString", "coordinates": [ [ 0.617874, 0.513074 ], [ 0.78245, 0.416642 ] ] } },
+{ "type": "Feature", "properties": { "ID": "9", "Tên_Ðuong": "0" }, "geometry": { "type": "LineString", "coordinates": [ [ 0.568032, 0.427139 ], [ 0.742987, 0.326992 ], [ 0.743936, 0.326066 ] ] } },
+{ "type": "Feature", "properties": { "ID": "10", "Tên_Ðuong": "Ðuong sô 7" }, "geometry": { "type": "LineString", "coordinates": [ [ 0.556919, 0.588078 ], [ 0.565212, 0.603788 ] ] } },
+{ "type": "Feature", "properties": { "ID": "11", "Tên_Ðuong": "0" }, "geometry": { "type": "LineString", "coordinates": [ [ 0.275198, 0.563533 ], [ 0.48218, 0.456864 ] ] } },
+{ "type": "Feature", "properties": { "ID": "0", "Tên_Ðuong": "0" }, "geometry": { "type": "LineString", "coordinates": [ [ 0.310021, 0.647641 ], [ 0.522712, 0.534489 ] ] } }
+]
+}
